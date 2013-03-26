def35593a1f35093d41031c2e6734b97ad43b457
Amol-Wankhede <amolswankhede@yahoo.com>
2013-03-26 17:27:44 +0530

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,10 +1,12 @@
-  $database = "sakila";
+	$host = "localhost";
+	$database = "sakila";
 	$username = "root";
 	$password = "root";
-		try {
-		    $connection = new PDO("mysql:host=localhost;dbname=$database", $username, $password);
-		    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    		return $connection;	
-		} catch(PDOException $e) {
-		    die( "<div class='alert alert-error'>ERROR : " . $e->getMessage() . "</div>");
- 		}
+	
+	try {
+	    $connection = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+	    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	return $connection;	
+	} catch(PDOException $e) {
+	    die( "<div class='alert alert-error'>ERROR : " . $e->getMessage() . "</div>");
+ 	}
